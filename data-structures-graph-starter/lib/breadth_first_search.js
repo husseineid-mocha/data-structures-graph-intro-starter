b5496e56208e4e4b95dc93f3afffc4bb0eb23d18
Hussein Eid <husseineid@mochamacbook.myfiosgateway.com>
2020-12-30 16:11:37 -0500

    max val

--- data-structures-graph-starter/lib/breadth_first_search.js
+++ data-structures-graph-starter/lib/breadth_first_search.js
@@ -1,5 +1,21 @@
 function breadthFirstSearch(startingNode, targetVal) {
+    let queue = [startingNode]
+    let visited = new Set()
 
+    while (queue.length) {
+        let node = queue.shift()
+
+        if (visited.has(node)) continue
+        visited.add(node)
+
+        if (node.val === targetVal) return node
+
+        queue.push(...node.neighbors)
+            // node.neighbors.forEach((neighbor) => {
+            //     queue.push(neighbor.val)
+            // })
+    }
+    return null
 }
 
 module.exports = {
--- data-structures-graph-starter/lib/max_value.js
+++ data-structures-graph-starter/lib/max_value.js
@@ -1,6 +1,19 @@
-function maxValue(node, visited=new Set()) {
+function maxValue(node, visited = new Set()) {
 
+    visited.add(node.val)
+
+    node.neighbors.forEach((neighbor) => {
+        if (visited.has(neighbor.val)) return
+        maxValue(neighbor, visited)
+    })
+    let res = Array.from(visited)
+    console.log(res)
+    return Math.max(...res)
 }
+
+// if visisted has a node we need to return SOMETHING
+// add node to visited
+//for the neighbors of node we want to 
 
 module.exports = {
     maxValue

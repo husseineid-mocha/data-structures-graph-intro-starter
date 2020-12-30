593a459b035cdbefef3804c33f00aae483337764
Hussein Eid <husseineid@mochamacbook.myfiosgateway.com>
2020-12-30 13:36:59 -0500

    changes

--- problems/graph.js
+++ problems/graph.js
@@ -1,15 +1,20 @@
 class Graph {
     constructor() {
-        // Code goes here ...
+        this.adjList = new Object();
     }
 
-
     addVertex(vertex) {
-        // Code goes here ...
+        if (!this.adjList[vertex]) {
+            this.adjList[vertex] = new Array();
+        } else return;
     }
 
     addEdges(srcValue, destValue) {
-        // Code goes here ...
+        if (!this.adjList[srcValue]) this.addVertex(srcValue)
+        if (!this.adjList[destValue]) this.addVertex(destValue)
+
+        this.adjList[srcValue].push(destValue)
+        this.adjList[destValue].push(srcValue)
     }
 
     buildGraph(edges) {

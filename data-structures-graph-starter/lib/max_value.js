b0ce6897e18bbd8122d57660ec483a52314001d2
raymondmay95 <raymond.may.95@outlook.com>
2020-12-30 11:59:25 -1000

    finished numregions

--- data-structures-graph-starter/lib/max_value.js
+++ data-structures-graph-starter/lib/max_value.js
@@ -1,20 +1,18 @@
 function maxValue(node, visited = new Set()) {
+  visited.add(node.val);
 
-    visited.add(node.val)
-
-    node.neighbors.forEach((neighbor) => {
-        if (visited.has(neighbor.val)) return
-        maxValue(neighbor, visited)
-    })
-    let res = Array.from(visited)
-    console.log(res)
-    return Math.max(...res)
+  node.neighbors.forEach((neighbor) => {
+    if (visited.has(neighbor.val)) return;
+    maxValue(neighbor, visited);
+  });
+  let res = Array.from(visited);
+  return Math.max(...res);
 }
 
 // if visisted has a node we need to return SOMETHING
 // add node to visited
-//for the neighbors of node we want to 
+//for the neighbors of node we want to
 
 module.exports = {
-    maxValue
+  maxValue,
 };
--- data-structures-graph-starter/lib/num_regions.js
+++ data-structures-graph-starter/lib/num_regions.js
@@ -1,7 +1,24 @@
 function numRegions(graph) {
+  let count = 0;
+  let visited = new Set();
+  for (let key in graph) {
+    if (!visited.has(key)) {
+      _dsearch(graph, key, visited);
+      count++;
+    }
+  }
+  return count;
+}
 
+function _dsearch(graph, node, visited) {
+  if (visited.has(node)) return;
+  else visited.add(node);
+
+  graph[node].forEach((neighbor) => {
+    _dsearch(graph, neighbor, visited);
+  });
 }
 
 module.exports = {
-    numRegions
+  numRegions,
 };
